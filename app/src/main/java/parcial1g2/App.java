6515107258619cb5800b0d6e7b8d83f831de1ac6
JuanAgogoo <juan.arangoo@upb.edu.co>
2024-08-23 13:50:05 -0500

    Update App.java
    Juan Jose Arango Ocampo 
    L.Programación 12-2

--- app/src/main/java/parcial1g2/App.java
+++ app/src/main/java/parcial1g2/App.java
@@ -1,13 +1,4 @@
-
-package parcial1g2;
-
-
-public class App {
-
-
-    
-
-     public static void main(String[] args) {
+ public static void main(String[] args) {
 
         Scanner Juan = new Scanner(System.in);
         int cantidadVehiculoss = 0;
@@ -112,28 +103,3 @@ public class App {
     }
 
     }
-    
-   
-        
-        
-
-    }
-   
-
-
-// Logica de solución
-    /*  Implemente una funcion llamada calcularTotalImpuestos que cumpla con las siguientes caracteristicas
-        * Recibe: 
-            - Una cadena de caracteres que representa la decada (70, 80, 90, 00, 10, 20, Otra)
-            - Un entero que representa avaluo del vehiculo
-        * Retorna:
-            - Un entero representando el valor total a pagar de los impuestos
-        * Controlar errores retornando -1
-    */
-    //------------------------------------------------------------------
-    
-
-
-    //------------------------------------------------------------------
-
-
